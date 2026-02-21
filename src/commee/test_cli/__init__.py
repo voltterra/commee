3a481a01318406b7758b92916a7ca5b601373c03
Dmitry Aminev <voltterra@users.noreply.github.com>
2026-02-21 01:12:49 +0100

    feature(wip): Add WhatsApp setup...

--- src/commee/test_cli/__init__.py
+++ src/commee/test_cli/__init__.py
@@ -4,6 +4,7 @@ import click
 from dotenv import load_dotenv
 
 from commee.messengers.telegram import TelegramMessenger
+from commee.messengers.whatsapp import WhatsAppMessenger
 
 load_dotenv()
 
@@ -18,6 +19,18 @@ def cli():
 def run_tg(message: str):
     app = TelegramMessenger(os.getenv("TG_APP_TOKEN", ""))
     chat_id = os.getenv("TG_CHAT_ID", "")
+    app.send_message(chat_id, message)
+    app.run(lambda cid, text: print(f"[{cid}]: {text}"))
+
+
+@cli.command("wa")
+@click.argument("message")
+def run_wa(message: str):
+    app = WhatsAppMessenger(
+        token=os.getenv("WA_TOKEN", ""),
+        phone_number_id=os.getenv("WA_PHONE_NUMBER_ID", ""),
+    )
+    chat_id = os.getenv("WA_CHAT_ID", "")
     app.send_message(chat_id, message)
     app.run(lambda cid, text: print(f"[{cid}]: {text}"))
 
